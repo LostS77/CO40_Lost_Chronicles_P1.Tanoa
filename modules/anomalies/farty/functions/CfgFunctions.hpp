12c674f72821df7dcc871752f7818978efad6c3d
Billy S. <straderb@mindspring.com>
2024-07-24 15:30:07 -0400

    Fixed Farty

--- modules/anomalies/farty/functions/CfgFunctions.hpp
+++ modules/anomalies/farty/functions/CfgFunctions.hpp
@@ -2,9 +2,5 @@ class COMPONENT {
 	tag = COMPONENT;
 	class FARTY {
 		file = "modules\anomalies\farty\functions\farty";
-		class create_puddle {};
-		class avoid_fart {};
-		class puddle_trail {};
-		class dir_speed {};
 	};
 };
